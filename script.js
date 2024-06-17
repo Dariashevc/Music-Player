3b58ba13ad98347fa2bb2bcc97b5525041740c92
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2024-06-17 09:12:07 -0600

    commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+// Song titles
+const songs = ['Happy Together', 'Lost In Paradise', 'While You Wait For The Others'];
