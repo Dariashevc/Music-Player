5756670e24859e73d4be8705b9bf326ed83b699e
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2024-06-19 07:56:27 -0600

    commit

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ const nextBtn = document.querySelector('#next');
 const audio = document.querySelector('#audio');
 const progress = document.querySelector('#progress');
 const progressContainer = document.querySelector('#progress-container');
-const title = document.getElementById('title');
-const cover = document.getElementById('cover');
-const currTime = document.querySelector('#currTime');
-const durTime = document.querySelector('#durTime');
+const title = document.querySelector('#title');
+const cover = document.getElementById('#cover');
 
-// Song titles
-const songs = ['Happy Together', 'Lost In Paradise', 'While You Wait For The Others'];
+const songs = ['Happy Together', 'Snap Out Of It', 'While You Wait For The Others', 'The Sweet Escape', 'Lost In Paradise', 'Everyone Adores You At Least I Do',
+	'Nobody', 'Good Old-Fashioned Lover Boy', 'Hidden In The Sand', 'The Blonde'];
 
+let songIndex = 0;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
