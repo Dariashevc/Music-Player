2bca663eb32a8fa2909b2cb0f777d5934375f5e2
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2024-06-20 09:15:06 -0600

    commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const audio = document.querySelector('#audio');
 const progress = document.querySelector('#progress');
 const progressContainer = document.querySelector('#progress-container');
 const title = document.querySelector('#title');
-const cover = document.getElementById('#cover');
+const cover = document.querySelector('#cover');
 
 const songs = ['Happy Together', 'Snap Out Of It', 'While You Wait For The Others', 'The Sweet Escape', 'Lost In Paradise', 'Everyone Adores You At Least I Do',
 	'Nobody', 'Good Old-Fashioned Lover Boy', 'Hidden In The Sand', 'The Blonde'];
@@ -17,6 +17,6 @@ loadSong(songs[songIndex]);
 
 function loadSong(song) {
   title.innerText = song;
-  audio.src = `music/${song}.mp3`;
-  cover.src = `images/${song}.jpg`;
+  audio.src = `/music/${song}.mp3`;
+  cover.src = `/images/${song}.jpg`;
 }
